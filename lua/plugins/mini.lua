87d7ebd7a78b6e070ff23facb9d9589de333fffa
Denis Pechenkin <dpech0511@gmail.com>
2025-11-22 15:23:38 -0700

    Enable mini.surround
    - Enabled a plugin for surrounding selections with character pairs
      (e.g brackets, braces, etc.).
    - Configured surroundings for "{", "[",
      "(" and "<" characters. By default, these surroundings have whitespace
      padding which I don't really like. My edited configs remove the
      padding.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,5 +5,13 @@ return {
   config = function()
     require("mini.icons").setup()
     require("mini.pairs").setup()
+    require("mini.surround").setup({
+      custom_surroundings = {
+        ['('] = {output = {left = '(', right = ')'}},
+        ['['] = {output = {left = '[', right = ']'}},
+        ['{'] = {output = {left = '{', right = '}'}},
+        ['<'] = {output = {left = '<', right = '>'}},
+      }
+    })
   end
 }
