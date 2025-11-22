1cb029920f6026596a31d077f101d52485b1ed8a
Denis Pechenkin <dpech0511@gmail.com>
2025-11-22 14:56:02 -0700

    Set keymaps for saving and indenting
    - Set "<C-s>" to save the contents of the current buffer. It will save
      the file only if its contents have changed since the last save.
    - Set ">" to indent the selection by 1 shiftwidth and retain the
      selection.
    - Set "<" to reduce the indentation by 1 shiftwidth and retain the
      selection.

--- lua/keymaps/global.lua
+++ lua/keymaps/global.lua
@@ -1,5 +1,26 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 	
--- SET jj: switch INSERT to NORMAL 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", {})
+-- INSERT "jj" - switch INSERT to NORMAL 
+vim.keymap.set("i", "jj", "<Esc>", {noremap=true})
+
+-- NORMAL "<C-s>" - save the current buffer
+vim.keymap.set(
+  "n", 
+  "<C-s>", 
+  function() 
+    local cur_buf = vim.api.nvim_get_current_buf()
+    if vim.bo[cur_buf].modified == true then       
+      vim.cmd("w")
+    else
+      print("No changes detected in the current buffer. Nothing to save.")
+    end
+  end, 
+  {silent=true}
+)
+
+-- VISUAL ">" - indent by 1 shiftwidth and retain the selection
+vim.keymap.set("v", ">", ">gv$", {noremap=true})
+
+-- VISUAL "<" - unindent by 1 shiftwidth and retain the selection
+vim.keymap.set("v", "<", "<gv$", {noremap=true})
