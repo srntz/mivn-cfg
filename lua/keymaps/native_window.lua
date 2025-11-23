410e5a3f3de59315f0ac01eda98b7732010f2ccb
Denis Pechenkin <dpech0511@gmail.com>
2025-11-23 12:51:36 -0700

    Remap standard window key mappings
    - Changed the prefix of keymaps that switch the currently focused window
      from "<C-w>" to "<leader>w". The default counterparts of these
      mappings got disabled. Same happened to the "rotate" mapping.
    - Changed the keymaps for splitting the currently focused window from
      "<leader>wh" and "<leader>wv" to "<leader>wsh" and "<leader>wsv"
      (stands for "window split horizontal" and "window split vertical"
      respectively).

--- lua/keymaps/native_window.lua
+++ lua/keymaps/native_window.lua
@@ -1,17 +1,60 @@
--- SET <leader>wh: Split current window horizontally
+-- Keymap config for window-related commands. 
+--
+-- All window keymaps start with "<leader>w" for consistency. Some default mappings 
+-- are left untouched, which means they're still mapped to "<C-w>". These untouched 
+-- mappings are not currenly in active use, however, if any new default mappings come 
+-- into play,they would be remapped to "<leader>w".
+
+-- Unmap the listed keys from "<C-w>" keymaps. These mappings are remapped to 
+-- different keys later in the config. Keys that aren't included in the list
+-- stay mapped to "<C-w>".
+local unmap = {
+  -- split horizontally
+  "s", "<C-s>", "S",
+
+  -- split vertically
+  "v", "<C-v>",
+
+  -- switch windows
+  "h", "j", "k", "l",
+
+  -- rotate windows
+  "r"
+}
+for _, key in ipairs(unmap) do
+  vim.keymap.set("n", "<C-w>"..key, "<Nop>", {})
+end
+
+-- Map the listed keys to "<leader>w". These mappings match exactly match
+-- their default counterparts except for the prefix (i.e. switching to
+-- the window below is remapped from "<C-w>j" to "<leader>wj").
+local wincmds = {
+  h = "wincmd h",
+  j = "wincmd j",
+  k = "wincmd k",
+  l = "wincmd l",
+  r = "wincmd r",
+}
+for key, cmd in pairs(wincmds) do
+  vim.keymap.set("n", "<leader>w"..key, "<Cmd>"..cmd.."<CR>", {})
+end
+
+-- Map "<leader>wsh" (stands for "window split horizontal") to split the currently
+-- focused window horizontally.
 vim.keymap.set(
   "n", 
-  "<leader>wh", 
+  "<leader>wsh", 
   function()
     vim.api.nvim_open_win(0, true, {split="below", win=0})
   end,
   {}
 )
 
--- SET <leader>wv: Split current window vertically 
+-- Map "<leader>wsv" (stands for "window split vertical") to split the currently
+-- focused window vertically.
 vim.keymap.set(
   "n", 
-  "<leader>wv", 
+  "<leader>wsv", 
   function()
     vim.api.nvim_open_win(0, true, {split="right", win=0, vertical=true})
   end,
